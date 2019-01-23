80def2c6cc07138069ff46b87d3a7d023a178188
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2019-01-23 18:08:28 -0200

    test version 1.0

---
+++ background.js
@@ -0,0 +1,12 @@
+let options = {
+  type: "basic",
+  title: "Hora de bater o ponto!",
+  message: "Você ainda não bateu o ponto... entre no Ahgora e bata seu ponto!",
+  iconUrl: "images/ahgora.jpg"
+};
+
+function notifications() {
+  chrome.notifications.create(options)
+}
+
+setTimeout(notifications, 30000);
--- notifications.js
+++ notifications.js
@@ -1,20 +1,21 @@
-var options = {
+// Notifications
+
+let options = {
   type: "basic", 
   title: "Hora de bater o ponto!",
   message: "Entre no Ahgora e bata seu ponto!", 
   iconUrl: "images/ahgora.jpg"
 };
 
-
-// function clickHandler(e) {
-  chrome.notifications.create(options);
-  chrome.notifications.onClicked.addListener(redirect);
-// }
-
-function redirect() {
-  console.log("Teste de clique")
+function notifications(){
+  chrome.notifications.create(options)
 }
 
+function clickHandler(e) {
+  setTimeout(notifications, 5000);
+}
+
+// On Click
 
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('messageLink').addEventListener('click', clickHandler);
