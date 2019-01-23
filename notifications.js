2d57b31abf57e926b83489525150432647bf35fd
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2019-01-23 16:31:03 -0200

    custom notification

--- notifications.js
+++ notifications.js
@@ -1,15 +1,15 @@
 var options = {
   type: "basic", 
-  title: "My first Popup",
-  message: "Wow man!", 
-  iconUrl: "images/icon.png"
+  title: "Hora de bater o ponto!",
+  message: "Entre no Ahgora e bata seu ponto!", 
+  iconUrl: "images/ahgora.jpg"
 };
 
 
-function clickHandler(e) {
+// function clickHandler(e) {
   chrome.notifications.create(options);
   chrome.notifications.onClicked.addListener(redirect);
-}
+// }
 
 function redirect() {
   console.log("Teste de clique")
