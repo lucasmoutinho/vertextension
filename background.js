09035b94892f29ba08f910998e750bdb61c42b53
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2019-01-24 17:54:06 -0200

    image notifications

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 let optionsEntrance = {
-  type: "basic",
+  type: "image",
   title: "Hora de bater o ponto!",
   message: "Entre no Ahgora e bata seu ponto!",
-  iconUrl: "images/ahgora.jpg"
+  iconUrl: "images/ahgora.jpg",
+  imageUrl: "images/homer.jpg"
 };
 
 let optionsLunch = {
