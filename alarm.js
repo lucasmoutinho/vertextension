69a6f225545670b55895a257f9bf4a91e90abc8a
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2019-01-24 14:48:43 -0200

    alarm funfando

--- alarm.js
+++ alarm.js
@@ -1,8 +1,20 @@
 function clickHandler(e) {
-  let alarmName = 'FirstAlarm'
-  chrome.alarms.create(alarmName, {
-    when: Date.now() + 0.1, periodInMinutes: 0.5
-  });
+  let timeResponse = document.getElementById('entranceAlarm').value
+  if(timeResponse){
+    console.log(timeResponse)
+    let array = timeResponse.split(":")
+    console.log(array)
+    let todayDate = new Date(Date.now())
+    let alarmTime = new Date(todayDate.getFullYear(),todayDate.getMonth(),todayDate.getDate(),array[0],array[1],0,0)
+    if(todayDate.getTime() > alarmTime.getTime()){
+      alarmTime.setDate(alarmTime.getDate() + 1)
+    }
+    console.log(alarmTime);
+    let alarmName = 'FirstAlarm'
+    chrome.alarms.create(alarmName, {
+      when: alarmTime.getTime(), periodInMinutes: 1
+    });
+  }
 }
 
 // On Click
